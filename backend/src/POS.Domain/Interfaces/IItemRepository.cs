0ce96bafc74d4b20864d2cde56aa1ef5a995e2af
Elmer <varquez.elmerjr@gmail.com>
2026-06-13 20:20:36 +0800

    feat: add item repository

--- backend/src/POS.Domain/Interfaces/IItemRepository.cs
+++ backend/src/POS.Domain/Interfaces/IItemRepository.cs
@@ -5,8 +5,8 @@ namespace POS.Domain.Interfaces;
 public interface IItemRepository
 {
     Task<Item?> GetByIdAsync(Guid id, CancellationToken ct = default);
-    Task<List<Item>> GetAllAsync(CancellationToken ct = default);
-    Task<List<Item>> GetLowStockAsync(CancellationToken ct = default);
+    Task<IList<Item>> GetAllAsync(CancellationToken ct = default);
+    Task<IList<Item>> GetLowStockAsync(CancellationToken ct = default);
     Task AddAsync(Item item, CancellationToken ct = default);
     Task UpdateAsync(Item item, CancellationToken ct = default);
     Task DeleteAsync(Guid id, CancellationToken ct = default);
---
+++ backend/src/POS.Infrastructure/Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
+using POS.Domain.Interfaces;
+
+namespace POS.Infrastructure.Persistence.Repositories;
+
+public class ItemRepository : IItemRepository
+{
+    private readonly AppDbContext _context;
+
+    public ItemRepository(AppDbContext context) => _context = context;
+
+    public async Task<Item?> GetByIdAsync(Guid id, CancellationToken ct = default)
+        => await _context.Items
+            .Include(i => i.Category)
+            .FirstOrDefaultAsync(i => i.Id == id, ct);
+
+    public async Task<IList<Item>> GetAllAsync(CancellationToken ct = default)
+        => await _context.Items
+            .Include(i => i.Category)
+            .Where(i => i.IsActive)
+            .OrderBy(i => i.Name)
+            .ToListAsync(ct);
+
+    public async Task<IList<Item>> GetLowStockAsync(CancellationToken ct = default)
+        => await _context.Items
+            .Include(i => i.Category)
+            .Where(i => i.IsActive && i.Stock <= i.LowStockThreshold)
+            .ToListAsync(ct);
+
+    public async Task AddAsync(Item item, CancellationToken ct = default)
+        => await _context.Items.AddAsync(item, ct);
+
+    public Task UpdateAsync(Item item, CancellationToken ct = default)
+    {
+        _context.Items.Update(item);
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken ct = default)
+    {
+        var item = await _context.Items.FindAsync(new object[] { id }, ct);
+        if (item is not null) _context.Items.Remove(item);
+    }
+}
