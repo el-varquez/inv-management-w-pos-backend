cba66db15e3561295bda695fd6f8618c61d49857
Elmer Varquez <83869062+el-varquez@users.noreply.github.com>
2026-08-13 14:19:43 +0800

    feat!: delete Platform (SuperAdmin) module

--- src/POS.Application/Platform/Commands/ReactivateTenant/ReactivateTenantCommand.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace POS.Application.Platform.Commands.ReactivateTenant;
-
-public record ReactivateTenantCommand(Guid Id) : IRequest;
--- src/POS.Application/Platform/Commands/SuspendTenant/SuspendTenantCommand.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace POS.Application.Platform.Commands.SuspendTenant;
-
-public record SuspendTenantCommand(Guid Id) : IRequest;
